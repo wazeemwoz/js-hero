e9880d36e914f8d62a89b560e49674a328d847b3
wazeemwoz <wazeemwoz@gmail.com>
2021-03-22 17:35:18 +0000

    add to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 const windmill = require('@windmill/react-ui/config')
-module.exports = {
+module.exports = windmill({
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -14,4 +14,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+})
