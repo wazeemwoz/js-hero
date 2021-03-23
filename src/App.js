f28560c697e8e9cc7efb8e0fd40d96e53c1de425
wazeemwoz <wazeemwoz@gmail.com>
2021-03-23 00:14:07 +0000

    center title

--- src/App.js
+++ src/App.js
@@ -393,13 +393,7 @@ function Header(props) {
   const [isModalOpen, setModalOpen] = useState(false);
   const closeModal = () => { setModalOpen(false) }
   return (
-    <header className={`${mtheme.headerColorBg} content-center`}>
-      <nav className="justify-between w-full text-white p-2">
-        <a href="/"><span className="font-semibold text-xl tracking-tight">JS Hero</span></a>
-        <button className="float-right" onClick={() => { setModalOpen(true) }}>
-          <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-help-circle"><circle cx={12} cy={12} r={10} /><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" /><line x1={12} y1={17} x2="12.01" y2={17} /></svg>
-        </button>
-      </nav>
+    <>
       <SimpleModal
         isModalOpen={isModalOpen}
         closeModal={closeModal}
@@ -409,7 +403,15 @@ function Header(props) {
         <p className="py-2" >JS Hero is a coding game to help people practice coding concepts. Navigate the character to the target to win each level.</p>
         <p>Every level uses the same solution which means players have to build together an algorithm to eventually succeed.</p>
       </SimpleModal>
-    </header>
+      <header className={mtheme.headerColorBg}>
+        <nav className="w-full text-white p-2 grid grid-cols-3 justify-items-center">
+          <a href="/" className="col-start-2"><span className="font-semibold text-xl tracking-tight">JS Hero</span></a>
+          <button className="ml-auto" onClick={() => { setModalOpen(true) }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-help-circle"><circle cx={12} cy={12} r={10} /><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" /><line x1={12} y1={17} x2="12.01" y2={17} /></svg>
+          </button>
+        </nav>
+      </header>
+    </>
   )
 }
 
